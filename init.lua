66820c3b09369a9c86a7bc6335753cfa66358529
bosapara <38753015+bosapara@users.noreply.github.com>
2019-08-03 17:40:12 +0400

    minify code + craft tool adding

--- init.lua
+++ init.lua
@@ -1,17 +1,18 @@
+local w_note = "white_button.png"
+local w_pressed = "white_button_pressed.png"
+local b_note = "black_button.png"
+local b_pressed = "black_button_pressed.png"
+
+local form = "size[23.2,9] bgcolor[#08080866;true] box[-0.3,-0.2;23.6,7.8;#000000cc] image_button[0,0.1;1.2,7;"..w_note..";c3;;false;false;"..w_pressed.."] image_button[1.1,0.1;1.2,7;"..w_note..";d3;;false;false;"..w_pressed.."] image_button[2.2,0.1;1.2,7;"..w_note..";e3;;false;false;"..w_pressed.."] image_button[3.3,0.1;1.2,7;"..w_note..";f3;;false;false;"..w_pressed.."] image_button[4.4,0.1;1.2,7;"..w_note..";g3;;false;false;"..w_pressed.."] image_button[5.5,0.1;1.2,7;"..w_note..";a3;;false;false;"..w_pressed.."] image_button[6.6,0.1;1.2,7;"..w_note..";b3;;false;false;"..w_pressed.."] image_button[7.7,0.1;1.2,7;"..w_note..";c1;;false;false;"..w_pressed.."] image_button[8.8,0.1;1.2,7;"..w_note..";d1;;false;false;"..w_pressed.."] image_button[9.9,0.1;1.2,7;"..w_note..";e1;;false;false;"..w_pressed.."] image_button[11,0.1;1.2,7;"..w_note..";f1;;false;false;"..w_pressed.."] image_button[12.1,0.1;1.2,7;"..w_note..";g1;;false;false;"..w_pressed.."] image_button[13.2,0.1;1.2,7;"..w_note..";a1;;false;false;"..w_pressed.."] image_button[14.3,0.1;1.2,7;"..w_note..";b1;;false;false;"..w_pressed.."] image_button[15.4,0.1;1.2,7;"..w_note..";c2;;false;false;"..w_pressed.."] image_button[16.5,0.1;1.2,7;"..w_note..";d2;;false;false;"..w_pressed.."] image_button[17.6,0.1;1.2,7;"..w_note..";e2;;false;false;"..w_pressed.."] image_button[18.7,0.1;1.2,7;"..w_note..";f2;;false;false;"..w_pressed.."] image_button[19.8,0.1;1.2,7;"..w_note..";g2;;false;false;"..w_pressed.."] image_button[20.9,0.1;1.2,7;"..w_note..";a2;;false;false;"..w_pressed.."] image_button[22,0.1;1.2,7;"..w_note..";b2;;false;false;"..w_pressed.."] image_button[0.6,0.1;1,4.5;"..b_note..";c3_sharp;;false;false;"..b_pressed.."]  image_button[1.75,0.1;1,4.5;"..b_note..";d3_sharp;;false;false;"..b_pressed.."] image_button[3.9,0.1;1,4.5;"..b_note..";f3_sharp;;false;false;"..b_pressed.."] image_button[5.05,0.1;1,4.5;"..b_note..";g3_sharp;;false;false;"..b_pressed.."] image_button[6.15,0.1;1,4.5;"..b_note..";a3_sharp;;false;false;"..b_pressed.."] image_button[8.3,0.1;1,4.5;"..b_note..";c1_sharp;;false;false;"..b_pressed.."] image_button[9.45,0.1;1,4.5;"..b_note..";d1_sharp;;false;false;"..b_pressed.."] image_button[11.6,0.1;1,4.5;"..b_note..";f1_sharp;;false;false;"..b_pressed.."] image_button[12.75,0.1;1,4.5;"..b_note..";g1_sharp;;false;false;"..b_pressed.."] image_button[13.85,0.1;1,4.5;"..b_note..";a1_sharp;;false;false;"..b_pressed.."] image_button[15.95,0.1;1,4.5;"..b_note..";c2_sharp;;false;false;"..b_pressed.."] image_button[17.15,0.1;1,4.5;"..b_note..";d2_sharp;;false;false;"..b_pressed.."] image_button[19.3,0.1;1,4.5;"..b_note..";f2_sharp;;false;false;"..b_pressed.."] image_button[20.4,0.1;1,4.5;"..b_note..";g2_sharp;;false;false;"..b_pressed.."] image_button[21.5,0.1;1,4.5;"..b_note..";a2_sharp;;false;false;"..b_pressed.."]"
+
 minetest.register_chatcommand("piano", {
 	params = "",
 	description = "Piano keyboard",
 	privs = {basic_privs=true},
 	func = function(name, param)
 
-	local w_note = "white_button.png"
-	local w_pressed = "white_button_pressed.png"
-	local b_note = "black_button.png"
-	local b_pressed = "black_button_pressed.png"
+		minetest.show_formspec(name, "piano_keyboard", form)
 	
-	minetest.show_formspec(name, "piano_keyboard", "size[23.2,9] bgcolor[#08080866;true] box[-0.3,-0.2;23.6,7.8;#000000cc] image_button[0,0.1;1.2,7;"..w_note..";c3;;false;false;"..w_pressed.."] image_button[1.1,0.1;1.2,7;"..w_note..";d3;;false;false;"..w_pressed.."] image_button[2.2,0.1;1.2,7;"..w_note..";e3;;false;false;"..w_pressed.."] image_button[3.3,0.1;1.2,7;"..w_note..";f3;;false;false;"..w_pressed.."] image_button[4.4,0.1;1.2,7;"..w_note..";g3;;false;false;"..w_pressed.."] image_button[5.5,0.1;1.2,7;"..w_note..";a3;;false;false;"..w_pressed.."] image_button[6.6,0.1;1.2,7;"..w_note..";b3;;false;false;"..w_pressed.."] image_button[7.7,0.1;1.2,7;"..w_note..";c1;;false;false;"..w_pressed.."] image_button[8.8,0.1;1.2,7;"..w_note..";d1;;false;false;"..w_pressed.."] image_button[9.9,0.1;1.2,7;"..w_note..";e1;;false;false;"..w_pressed.."] image_button[11,0.1;1.2,7;"..w_note..";f1;;false;false;"..w_pressed.."] image_button[12.1,0.1;1.2,7;"..w_note..";g1;;false;false;"..w_pressed.."] image_button[13.2,0.1;1.2,7;"..w_note..";a1;;false;false;"..w_pressed.."] image_button[14.3,0.1;1.2,7;"..w_note..";b1;;false;false;"..w_pressed.."] image_button[15.4,0.1;1.2,7;"..w_note..";c2;;false;false;"..w_pressed.."] image_button[16.5,0.1;1.2,7;"..w_note..";d2;;false;false;"..w_pressed.."] image_button[17.6,0.1;1.2,7;"..w_note..";e2;;false;false;"..w_pressed.."] image_button[18.7,0.1;1.2,7;"..w_note..";f2;;false;false;"..w_pressed.."] image_button[19.8,0.1;1.2,7;"..w_note..";g2;;false;false;"..w_pressed.."] image_button[20.9,0.1;1.2,7;"..w_note..";a2;;false;false;"..w_pressed.."] image_button[22,0.1;1.2,7;"..w_note..";b2;;false;false;"..w_pressed.."] image_button[0.6,0.1;1,4.5;"..b_note..";c3_sharp;;false;false;"..b_pressed.."]  image_button[1.75,0.1;1,4.5;"..b_note..";d3_sharp;;false;false;"..b_pressed.."] image_button[3.9,0.1;1,4.5;"..b_note..";f3_sharp;;false;false;"..b_pressed.."] image_button[5.05,0.1;1,4.5;"..b_note..";g3_sharp;;false;false;"..b_pressed.."] image_button[6.15,0.1;1,4.5;"..b_note..";a3_sharp;;false;false;"..b_pressed.."] image_button[8.3,0.1;1,4.5;"..b_note..";c1_sharp;;false;false;"..b_pressed.."] image_button[9.45,0.1;1,4.5;"..b_note..";d1_sharp;;false;false;"..b_pressed.."] image_button[11.6,0.1;1,4.5;"..b_note..";f1_sharp;;false;false;"..b_pressed.."] image_button[12.75,0.1;1,4.5;"..b_note..";g1_sharp;;false;false;"..b_pressed.."] image_button[13.85,0.1;1,4.5;"..b_note..";a1_sharp;;false;false;"..b_pressed.."] image_button[15.95,0.1;1,4.5;"..b_note..";c2_sharp;;false;false;"..b_pressed.."] image_button[17.15,0.1;1,4.5;"..b_note..";d2_sharp;;false;false;"..b_pressed.."] image_button[19.3,0.1;1,4.5;"..b_note..";f2_sharp;;false;false;"..b_pressed.."] image_button[20.4,0.1;1,4.5;"..b_note..";g2_sharp;;false;false;"..b_pressed.."] image_button[21.5,0.1;1,4.5;"..b_note..";a2_sharp;;false;false;"..b_pressed.."]")
-	
-
 	end,
 })
 
@@ -23,21 +24,10 @@ minetest.register_tool("piano:tool", {
 	groups = {not_in_creative_inventory=1},
 	on_use = function(itemstack, user, pointed_thing)
 
-		local w_note = "white_button.png"
-		local w_pressed = "white_button_pressed.png"
-		local b_note = "black_button.png"
-		local b_pressed = "black_button_pressed.png"
-		
 		local name = user:get_player_name()
 	
-		minetest.show_formspec(name, "piano_keyboard", "size[23.2,9] bgcolor[#08080866;true] box[-0.3,-0.2;23.6,7.8;#000000cc] image_button[0,0.1;1.2,7;"..w_note..";c3;;false;false;"..w_pressed.."] image_button[1.1,0.1;1.2,7;"..w_note..";d3;;false;false;"..w_pressed.."] image_button[2.2,0.1;1.2,7;"..w_note..";e3;;false;false;"..w_pressed.."] image_button[3.3,0.1;1.2,7;"..w_note..";f3;;false;false;"..w_pressed.."] image_button[4.4,0.1;1.2,7;"..w_note..";g3;;false;false;"..w_pressed.."] image_button[5.5,0.1;1.2,7;"..w_note..";a3;;false;false;"..w_pressed.."] image_button[6.6,0.1;1.2,7;"..w_note..";b3;;false;false;"..w_pressed.."] image_button[7.7,0.1;1.2,7;"..w_note..";c1;;false;false;"..w_pressed.."] image_button[8.8,0.1;1.2,7;"..w_note..";d1;;false;false;"..w_pressed.."] image_button[9.9,0.1;1.2,7;"..w_note..";e1;;false;false;"..w_pressed.."] image_button[11,0.1;1.2,7;"..w_note..";f1;;false;false;"..w_pressed.."] image_button[12.1,0.1;1.2,7;"..w_note..";g1;;false;false;"..w_pressed.."] image_button[13.2,0.1;1.2,7;"..w_note..";a1;;false;false;"..w_pressed.."] image_button[14.3,0.1;1.2,7;"..w_note..";b1;;false;false;"..w_pressed.."] image_button[15.4,0.1;1.2,7;"..w_note..";c2;;false;false;"..w_pressed.."] image_button[16.5,0.1;1.2,7;"..w_note..";d2;;false;false;"..w_pressed.."] image_button[17.6,0.1;1.2,7;"..w_note..";e2;;false;false;"..w_pressed.."] image_button[18.7,0.1;1.2,7;"..w_note..";f2;;false;false;"..w_pressed.."] image_button[19.8,0.1;1.2,7;"..w_note..";g2;;false;false;"..w_pressed.."] image_button[20.9,0.1;1.2,7;"..w_note..";a2;;false;false;"..w_pressed.."] image_button[22,0.1;1.2,7;"..w_note..";b2;;false;false;"..w_pressed.."] image_button[0.6,0.1;1,4.5;"..b_note..";c3_sharp;;false;false;"..b_pressed.."]  image_button[1.75,0.1;1,4.5;"..b_note..";d3_sharp;;false;false;"..b_pressed.."] image_button[3.9,0.1;1,4.5;"..b_note..";f3_sharp;;false;false;"..b_pressed.."] image_button[5.05,0.1;1,4.5;"..b_note..";g3_sharp;;false;false;"..b_pressed.."] image_button[6.15,0.1;1,4.5;"..b_note..";a3_sharp;;false;false;"..b_pressed.."] image_button[8.3,0.1;1,4.5;"..b_note..";c1_sharp;;false;false;"..b_pressed.."] image_button[9.45,0.1;1,4.5;"..b_note..";d1_sharp;;false;false;"..b_pressed.."] image_button[11.6,0.1;1,4.5;"..b_note..";f1_sharp;;false;false;"..b_pressed.."] image_button[12.75,0.1;1,4.5;"..b_note..";g1_sharp;;false;false;"..b_pressed.."] image_button[13.85,0.1;1,4.5;"..b_note..";a1_sharp;;false;false;"..b_pressed.."] image_button[15.95,0.1;1,4.5;"..b_note..";c2_sharp;;false;false;"..b_pressed.."] image_button[17.15,0.1;1,4.5;"..b_note..";d2_sharp;;false;false;"..b_pressed.."] image_button[19.3,0.1;1,4.5;"..b_note..";f2_sharp;;false;false;"..b_pressed.."] image_button[20.4,0.1;1,4.5;"..b_note..";g2_sharp;;false;false;"..b_pressed.."] image_button[21.5,0.1;1,4.5;"..b_note..";a2_sharp;;false;false;"..b_pressed.."]")
-	
-
-
-	
-
-
-
+		minetest.show_formspec(name, "piano_keyboard", form)
+		
 	end,
 
 })
@@ -90,7 +80,45 @@ end
 
 
 
-
+local v = {
+	{"c3", "noteblock_c3"},
+	{"d3", "noteblock_d3"},
+	{"e3", "noteblock_e3"},
+	{"f3", "noteblock_f3"},
+	{"g3", "noteblock_g3"},
+	{"a3", "noteblock_a3"},
+	{"b3", "noteblock_b3"},
+	{"c1", "noteblock_c"},
+	{"d1", "noteblock_d"},
+	{"e1", "noteblock_e"},
+	{"f1", "noteblock_f"},
+	{"g1", "noteblock_g"},
+	{"a1", "noteblock_a"},
+	{"b1", "noteblock_b"},
+	{"c2", "noteblock_c2"},
+	{"d2", "noteblock_d2"},
+	{"e2", "noteblock_e2"},
+	{"f2", "noteblock_f2"},
+	{"g2", "noteblock_g2"},
+	{"a2", "noteblock_a2"},
+	{"b2", "noteblock_b2"},
+	{"c3_sharp", "noteblock_csharp3"},
+	{"d3_sharp", "noteblock_dsharp3"},
+	{"f3_sharp", "noteblock_fsharp3"},
+	{"g3_sharp", "noteblock_gsharp3"},
+	{"a3_sharp", "noteblock_asharp3"},
+	{"c2_sharp", "noteblock_csharp2"},
+	{"d2_sharp", "noteblock_dsharp2"},
+	{"f2_sharp", "noteblock_fsharp2"},
+	{"g2_sharp", "noteblock_gsharp2"},
+	{"a2_sharp", "noteblock_asharp2"},
+	{"c1_sharp", "noteblock_csharp"},
+	{"d1_sharp", "noteblock_dsharp"},
+	{"f1_sharp", "noteblock_fsharp"},
+	{"g1_sharp", "noteblock_gsharp"},
+	{"a1_sharp", "noteblock_asharp"},
+			
+}
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
 	if formname=="piano_keyboard" then
@@ -98,126 +126,24 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 		
 		local pos = player:get_pos()
 		
-		if fields.c3 then
-			minetest.sound_play("noteblock_c3", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.d3 then
-			minetest.sound_play("noteblock_d3", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.e3 then
-			minetest.sound_play("noteblock_e3", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.f3 then
-			minetest.sound_play("noteblock_f3", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.g3 then
-			minetest.sound_play("noteblock_g3", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.a3 then
-			minetest.sound_play("noteblock_a3", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.b3 then
-			minetest.sound_play("noteblock_b3", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-			
-		elseif fields.c1 then
-			minetest.sound_play("noteblock_c", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.d1 then
-			minetest.sound_play("noteblock_d", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.e1 then
-			minetest.sound_play("noteblock_e", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.f1 then
-			minetest.sound_play("noteblock_f", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.g1 then
-			minetest.sound_play("noteblock_g", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.a1 then
-			minetest.sound_play("noteblock_a", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.b1 then
-			minetest.sound_play("noteblock_b", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
+		for _, v in pairs(v) do
 
-		elseif fields.c2 then
-			minetest.sound_play("noteblock_c2", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.d2 then
-			minetest.sound_play("noteblock_d2", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.e2 then
-			minetest.sound_play("noteblock_e2", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.f2 then
-			minetest.sound_play("noteblock_f2", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.g2 then
-			minetest.sound_play("noteblock_g2", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.a2 then
-			minetest.sound_play("noteblock_a2", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.b2 then
-			minetest.sound_play("noteblock_b2", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-
-		elseif fields.c3_sharp then
-			minetest.sound_play("noteblock_csharp3", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.d3_sharp then
-			minetest.sound_play("noteblock_dsharp3", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.f3_sharp then
-			minetest.sound_play("noteblock_fsharp3", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.g3_sharp then
-			minetest.sound_play("noteblock_gsharp3", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.a3_sharp then
-			minetest.sound_play("noteblock_asharp3", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-
-		elseif fields.c1_sharp then
-			minetest.sound_play("noteblock_csharp", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.d1_sharp then
-			minetest.sound_play("noteblock_dsharp", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.f1_sharp then
-			minetest.sound_play("noteblock_fsharp", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.g1_sharp then
-			minetest.sound_play("noteblock_gsharp", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.a1_sharp then
-			minetest.sound_play("noteblock_asharp", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-
-		elseif fields.c2_sharp then
-			minetest.sound_play("noteblock_csharp2", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.d2_sharp then
-			minetest.sound_play("noteblock_dsharp2", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.f2_sharp then
-			minetest.sound_play("noteblock_fsharp2", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.g2_sharp then
-			minetest.sound_play("noteblock_gsharp2", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-		elseif fields.a2_sharp then
-			minetest.sound_play("noteblock_asharp2", {pos = pos, max_hear_distance = 10, gain = 1.0,})
-			particle_effect(pos)
-
+				if fields[v[1]] then
+					minetest.sound_play(v[2], {pos = pos, max_hear_distance = 10, gain = 1.0,})
+					particle_effect(pos)
+				end	
 		end
-
-
 
 	end
 end)
 
 
 
+minetest.register_craft({
+	output = 'piano:tool',
+	recipe = {
+		{'group:wood', 'default:gold_ingot', 'group:wood'},
+		{'group:wood', 'default:gold_ingot', 'group:wood'},
+		{'group:wood', 'group:wood', 'group:wood'},
+	}
+})
